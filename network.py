d9fe2cdb59d9576f6ef2152e54823e2f24ac562c
Terence Helbert <terencehelbert@gmail.com>
2026-06-27 12:28:59 +0200

    added network layers and training logic

--- network.py
+++ network.py
@@ -3,18 +3,20 @@ import random as rd
 import function 
 
 class Linear():
+    # fully connected layer 
     def __init__(self, n_in, n_out):
-        self.W = np.array([[rd.randint(-1000,1000)/1000 for i in range(n_in)] for j in range(n_out)])
-        self.b = np.zeros((n_out,1))
+        self.W = np.array([[rd.randint(-1000,1000)/1000 for i in range(n_in)] for j in range(n_out)]) # weight, initially randomize between -1 and 1, shape (n_out, n_in), 1 row for each output neurons, 1 column for each entry
+        self.b = np.zeros((n_out,1)) # bias
     def forward(self, a_prev):
-        self.a_prev = a_prev
+        self.a_prev = a_prev # keep for backprop
         return np.dot(self.W,a_prev) + self.b
     def backward(self, delta):
         self.dW = 1/self.a_prev.shape[1] * np.dot(delta, np.transpose(self.a_prev))
         self.db = 1/self.a_prev.shape[1] * np.sum(delta, keepdims = True)
-        return np.dot(np.transpose(self.W),delta)
+        return np.dot(np.transpose(self.W),delta) # to give to previous 
      
 class ReLU():
+    # layer ReLU
     def __init__(self):
         return
     def forward(self, z):
@@ -24,6 +26,7 @@ class ReLU():
         return delta*function.Relu_prime(self.z)
         
 class Sigmoid():
+    # layer sigmoid
     def __init__(self):
         return
     def forward(self, z):
@@ -33,13 +36,16 @@ class Sigmoid():
         return delta*function.sigmoid_prime(self.z)
     
 class Network():
+    # to arrange multiple layers 
     def __init__(self, layers):
         self.layers = layers 
     def forward(self, X):
+        # forward pass 
         for layer in self.layers:
             X = layer.forward(X)
         return X
     def backward(self, delta):
+        # back prop 
         for layer in reversed(self.layers):
             delta = layer.backward(delta)
     def update(self, lr):
